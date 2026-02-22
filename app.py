a2c3df8565bcca03e29fd175daf1187c13e78ca4
Raman Kumar Yadav <rmnkumrydv@gmail.com>
2026-02-22 13:09:32 +0530

    Update import path for ConversationBufferMemory

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain_groq import ChatGroq
 from langchain.callbacks.base import BaseCallbackHandler
